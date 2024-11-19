46cc40b499a515b0900c15d6982eb2bc5a65794f
andreafarina <andrea.farina@polimi.it>
2024-11-19 10:52:59 +0100

    Complete Exercise_1.m

--- Code/Exercise_1.m
+++ Code/Exercise_1.m
@@ -26,9 +26,9 @@ p1 = [30, 30, 30];
 
 %% 3. Set and run the simulation
 % optical properties
-mua = 0.01;             % absorption coefficient [mm-1]
-mus = 1.0;              % scattering coefficient [mm-1]
-g = 0.0;                  % anisotropy factor
+mua = 0.005;             % absorption coefficient [mm-1]
+mus = 5.0/10;              % scattering coefficient [mm-1]
+g = 0.8;                  % anisotropy factor
 n = 1.4;                % refractive index
 musp = mus * (1 - g);   % reduced scattering coefficient [mm-1]
 
@@ -49,14 +49,14 @@ cfg.srctype = 'pencil';
 % set the temporal data
 cfg.tstart = 0;
 cfg.tend = 5e-9;     % 5 ns
-cfg.tstep = 100e-12; % 100 ps
+cfg.tstep = 10e-12; % 10 ps
 % output data are on the nodes
 cfg.method = 'elem';
 
 % set homogeneous optical properties
 cfg.elemprop = ones(length(elem),1);
 % run the simulation
-fluence = mmclab(cfg);
+[fluence,~,~,~,traj] = mmclab(cfg);
 % calculate the CW fluence summing over the time dimension
 cw = sum(fluence.data,2);
 %% 4. Plot CW fluence
@@ -64,17 +64,28 @@ figure(3),plotmesh([node,log10(cw)],elem,'x>0'),xlabel('x'),ylabel('y'),zlabel('
 
 
 %% 5. plot TD flux fluence
+% num_gates = size(fluence.data,2);
+% for i = 1:num_gates
+%     td = fluence.data(:,i);
+%     title(['time gates = ',num2str(i)])
+%     figure(4),plotmesh([node,log10(td)],elem,'x>0'),xlabel('x'),ylabel('y'),zlabel('z'),
+%     colorbar,
+%     pause(0.5)
+% end
 
 
 %% 6. Place a detector and pick-up the fluence value
-
+detpos = [0,0,30];
 %find the nearest node to the detector
-
+idx = dsearchn(cfg.node,detpos);
 % pick-up fluence at the identified node
-
+tpsf = fluence.data(idx,:);
+tgrid = cfg.tstart:cfg.tstep:cfg.tend;
+t = tgrid(1:end-1) + cfg.tstep/2;
+figure(5),semilogy(t,tpsf),xlabel('time [s]'),ylabel('fluence rate [1/mm2 / ps]')
 %% 7. Plot photon trajectories
-%NtrajPlot = 10;
-%PlotPhotonTrajecetories(traj,NtrajPlot,p0,p1);
+NtrajPlot = 100;
+PlotPhotonTrajecetories(traj,NtrajPlot,p0,p1);
 
 
 
